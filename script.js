0bc90087de2f40f156fdd9a859794d6b159136e2
Beef Webster <c_goldsmith@yahoo.com>
2022-08-15 20:47:39 -0700

    finished password generation logic

--- script.js
+++ script.js
@@ -1,13 +1,59 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword(){
+  
+  let length = prompt("Please enter a desired password length (8 - 128)")
+  var set = ""
+
+  if(length < 8 || length > 128){
+    alert("Please input a valid password length.")
+    return ""
+  }
+  
+  let lower = confirm("Include lowercase characters?")
+  if(lower){
+    set = "abcdefghijklmnopqrstuvwxyz"
+  }
+
+  let upper = confirm("Include uppercase characters?")
+  if(upper){
+    set += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  }
+
+  let numeric = confirm("Include numeric characters?")
+  if(numeric){
+    set += "01234567489"
+  }
+
+  let special = confirm("Include special characters?")
+  if(special){
+    set += " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+  }
+
+  if(set === ""){
+    alert("Please select at least one character set.")
+    return ""
+  }
+
+  alert("Generating password containing") // finish this
+  
+  let result = ""
+  while(result.length < length){
+    let i = Math.floor(Math.random() * set.length)
+    result += set[i]
+  }
+  // bingo bango
+  return result
+} 
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
